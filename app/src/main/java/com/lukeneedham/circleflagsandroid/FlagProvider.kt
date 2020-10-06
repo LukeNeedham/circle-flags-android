9c027e6cff16fb2c5e71dce85e08261efce87252
Luke <lukeneedham@outlook.com>
2020-10-06 11:44:58 +0200

    Fix drawable lookup, add unknown flag functions

--- app/src/main/java/com/lukeneedham/circleflagsandroid/FlagProvider.kt
+++ app/src/main/java/com/lukeneedham/circleflagsandroid/FlagProvider.kt
@@ -8,12 +8,36 @@ object FlagProvider {
     private const val DRAWABLE_TYPE = "drawable"
     private const val FLAG_NAME_PREFIX = "flag_"
 
-    fun getFlagResourceFromCountryAlpha2Code(alpha2Code: String, context: Context): Drawable? {
-        val resourceId = context.resources.getIdentifier(
+    /**
+     * @return the flag resource id for [alpha2Code],
+     * or 0 (the invalid resource id) if no flag is found
+     */
+    fun getFlagResIdFromCountryAlpha2Code(alpha2Code: String, context: Context): Int {
+        return context.resources.getIdentifier(
             FLAG_NAME_PREFIX + alpha2Code,
             DRAWABLE_TYPE,
             context.packageName
         )
+    }
+
+    /**
+     * @return the flag drawable for [alpha2Code],
+     * or null if no flag is found
+     */
+    fun getFlagFromCountryAlpha2Code(alpha2Code: String, context: Context): Drawable? {
+        val resourceId = getFlagResIdFromCountryAlpha2Code(alpha2Code, context)
+        if (resourceId == 0) {
+            return null
+        }
         return ContextCompat.getDrawable(context, resourceId)
+    }
+
+    fun getUnknownFlagResId(): Int {
+        return R.drawable.flag_xx
+    }
+
+    fun getUnknownFlag(context: Context): Drawable {
+        return ContextCompat.getDrawable(context, getUnknownFlagResId())
+            ?: error("The unknown flag is missing")
     }
 }
