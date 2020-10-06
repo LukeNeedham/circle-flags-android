85f667027bb30b998c6be7e7c31c17d411a3646e
Luke <lukeneedham@outlook.com>
2020-10-06 11:25:47 +0200

    Rename drawables to prefix name

--- app/src/main/java/com/lukeneedham/circleflagsandroid/FlagProvider.kt
+++ app/src/main/java/com/lukeneedham/circleflagsandroid/FlagProvider.kt
@@ -6,10 +6,11 @@ import androidx.core.content.ContextCompat
 
 object FlagProvider {
     private const val DRAWABLE_TYPE = "drawable"
+    private const val FLAG_NAME_PREFIX = "flag_"
 
     fun getFlagResourceFromCountryAlpha2Code(alpha2Code: String, context: Context): Drawable? {
         val resourceId = context.resources.getIdentifier(
-            alpha2Code,
+            FLAG_NAME_PREFIX + alpha2Code,
             DRAWABLE_TYPE,
             context.packageName
         )
